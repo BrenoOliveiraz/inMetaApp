6e2faeb85fbfeda2217501be9fe987440c3e3308
Breno Oliveira <brenooliveiraa@gmail.com>
2026-03-11 00:05:37 -0300

    feat: trigger para syncar os dados quando recupearar a conexao

---
+++ src/hooks/useSync.ts
@@ -0,0 +1,15 @@
+import NetInfo from "@react-native-community/netinfo"
+import { useEffect } from "react"
+import { syncWorkOrders } from "../services/syncService"
+
+export function useSync() {
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener((state) => {
+      if (state.isConnected) {
+        syncWorkOrders()
+      }
+    })
+
+    return unsubscribe
+  }, [])
+}
